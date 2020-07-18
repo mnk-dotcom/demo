e4531f9cac7651d3749fb3b1721c084d1955896d
Abdennasser ESSATI <abdennasser.essati@4d.com>
2020-07-18 15:21:26 +0100

    chore: add pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { FirstTopicComponent } from './first-topic/first-topic.component';
+import { SecondTopicComponent } from './second-topic/second-topic.component';
+import { ThirdTopicComponent } from './third-topic/third-topic.component';
 
 const routes: Routes = [
 	{
@@ -11,6 +14,18 @@ const routes: Routes = [
 	{
 		path: 'home',
 		component: HomeComponent
+	},
+	{
+		path: 'first-topic',
+		component: FirstTopicComponent
+	},
+	{
+		path: 'second-topic',
+		component: SecondTopicComponent
+	},
+	{
+		path: 'third-topic',
+		component: ThirdTopicComponent
 	},
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { FirstTopicComponent } from './first-topic/first-topic.component';
+import { SecondTopicComponent } from './second-topic/second-topic.component';
+import { ThirdTopicComponent } from './third-topic/third-topic.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    FirstTopicComponent,
+    SecondTopicComponent,
+    ThirdTopicComponent
   ],
   imports: [
     BrowserModule,
