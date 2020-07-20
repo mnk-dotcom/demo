2aac08e1c5fc01db6daf94b4f95e2bbe3dddd334
Abdennasser ESSATI <abdennasser.essati@4d.com>
2020-07-20 20:59:15 +0100

    chore: Fill data from backend

--- src/app/auth/auth-service.service.ts
+++ src/app/auth/auth-service.service.ts
@@ -18,86 +18,14 @@ export class AuthServiceService {
 
 	public login(username, password) {
 		this.http.post(`${this.url}/api/users/login`, { login: username, password }).subscribe(
-			result => {
-				this.user = {
-					id: 1,
-					nom: "administrateur",
-					prenom: "admin",
-					login: "admin",
-					password: "admin",
-					passwordinbyte: null,
-					profils: [
-						{
-							id: 1,
-							name: "Administrateur",
-							privileges: [
-								{
-									id: 1,
-									name: "Utilisateur"
-								},
-								{
-									id: 2,
-									name: "Profil"
-								},
-								{
-									id: 3,
-									name: "Projet"
-								},
-								{
-									id: 4,
-									name: "Env"
-								}
-							]
-						}
-					],
-					datecreation: null,
-					mail: "admin@test.com",
-					tel: "06613444444",
-					token: "20nasser!mustafa!khalid20"
-				}
+			(result: IUser) => {
+				this.user = result;
 				localStorage.setItem('auth-token', this.user.token);
 				this.router.navigate(['/home']);
+				alert('You have successfully logged in');
 			},
 			error => {
-				this.user = {
-					id: 1,
-					nom: "administrateur",
-					prenom: "admin",
-					login: "admin",
-					password: "admin",
-					passwordinbyte: null,
-					profils: [
-						{
-							id: 1,
-							name: "Administrateur",
-							privileges: [
-								{
-									id: 1,
-									name: "Utilisateur"
-								},
-								{
-									id: 2,
-									name: "Profil"
-								},
-								{
-									id: 3,
-									name: "Projet"
-								},
-								{
-									id: 4,
-									name: "Env"
-								}
-							]
-						}
-					],
-					datecreation: null,
-					mail: "admin@test.com",
-					tel: "06613444444",
-					token: "20nasser!mustafa!khalid20"
-				}
-				localStorage.setItem('auth-token', this.user.token);
-				localStorage.setItem('current-user', JSON.stringify(this.user));				
-				this.router.navigate(['/home']);
+				alert('Oups something went wrong');
 			})
 	}
 
